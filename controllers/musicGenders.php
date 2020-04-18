af09a3a92f2f7548cc53f01063bffbf8c979358e
DavidFernandezGuerreiro <davidferguer99@gmail.com>
2020-04-18 16:41:13 +0200

    Create endpoint to get idFestivals by gender music

--- controllers/musicGenders.php
+++ controllers/musicGenders.php
@@ -42,6 +42,25 @@ function musicGenders($task, $conn){
                 $data['count'] = 0;
             }
             break;
+        case 'get_festival_by_gender':
+            $id_music_gender = json_decode(file_get_contents("php://input"), true);
+            $id_music_gender = $id_music_gender['id_gender'];
+            if(isset($id_music_gender)){
+                $sql_festivals = "SELECT idFestival FROM fest_festival_music_gender WHERE idMusicGender = ". $id_music_gender;
+                $result = $conn->query($sql_festivals);
+                $resultArray = array();
+                foreach($result as $results){
+                    $resultArray[] = $results;
+                }
+
+                $data['results'] = $resultArray;
+                $data['count'] = count($resultArray);
+            }else{
+                $data['error'] = 'Error. Faltan parámentros. No se ha enviado el id del género musical.';
+                $data['results'] = null;
+                $data['count'] = 0;
+            }
+            break;
         default:
             break;
     }

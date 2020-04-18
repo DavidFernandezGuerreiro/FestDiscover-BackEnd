8ded2c12b5b7d8df6a7196344d09ec344c0d0ba8
DavidFernandezGuerreiro <davidferguer99@gmail.com>
2020-04-18 16:42:06 +0200

    Create endpoint to get festivals by gender music, country, min price and max price

--- controllers/festivals.php
+++ controllers/festivals.php
@@ -42,6 +42,38 @@ function festivals($task, $conn){
                 $data['count'] = 0;
             }
             break;
+        case 'get_festivals_by_filter':
+            $filters = json_decode(file_get_contents("php://input"), true);
+            $country = isset($filters['country']) ? $filters['country'] : '';
+            $min_price = isset($filters['min_price']) ? $filters['min_price'] : '';
+            $max_price = isset($filters['max_price']) ? $filters['max_price'] : '';
+
+            $result_festivals = musicGenders('get_festival_by_gender', $conn);
+            $id_festivals = $result_festivals['results'];
+
+
+                if($country != '' && $min_price != '' && $max_price != ''){
+                    $resultArray = array();
+                    foreach($id_festivals as $val) {
+                        $id_festival = intval($val['idFestival']);
+                        $sql_festival = "
+                        SELECT * FROM fest_festivals 
+                        WHERE id = " . $id_festival . " AND country = '" . $country . "' AND price >= " . $min_price . " AND price <= " . $max_price;
+                        $result = $conn->query($sql_festival);
+
+                        foreach($result as $results){
+                            array_push($resultArray, $results);
+                        }
+                    }
+                    $data['results'] = $resultArray;
+                    $data['count'] = count($resultArray);
+                }else{
+                    $data['error'] = 'Error. Faltan parámentros. Debe introducir todos los campos.';
+                    $data['results'] = null;
+                    $data['count'] = 0;
+                }
+
+            break;
         default:
             break;
     }
